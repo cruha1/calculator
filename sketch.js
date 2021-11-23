f332e0386e7abb8df7b9a02c5c9f6ab00c331bc6
cruha1 <54993799+cruha1@users.noreply.github.com>
2021-11-23 10:11:58 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,67 +1,89 @@
 function setup() {
-  createCanvas(400, 600);
+  createCanvas(325, 600);
 }
 
 function draw() {
   background(220);
   drawCalculator();
-  textSize(30);
+  var numDigits = floor(textWidth(display)/textWidth("1"));
   fill(0,0,0);
-//  +textWidth(numbers[numbers.length-1])   width of the latest num
-  text(numbers[0], 40,115)
-  text(numbers[1], 60+(1.10*textWidth(numbers[numbers.length-2])),115)
-  textSize(15);
-  text(exponent, 40+(2*textWidth(numbers[0])), 100)
-  textSize(30);
-  text(functions, 40+(1.10*textWidth(numbers[0])), 115)
+  if (textWidth(display) > 150){
+    for(var i = 0; i < numDigits/14; i += 0.0009)
+    textSize(30-i*7)
+  }else{
+    textSize(30);
+  }
+  text(display, 40, 115)
+  
+  text(answer, 250 ,155)
 }
 var numbers = [""];
-var functions = "";
-var exponent = "";
+var functions = [];
+var display = "";
+var answer
 mouseClicked = function() {
         if (zero.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "0";
+            display += "0";
         } else if (one.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "1";
+          display += "1";
         } else if (two.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "2";
+          display += "2";
         } else if (three.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "3";
+          display += "3";
         }else if (four.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "4";
+          display += "4";
         }else if (five.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "5";
+          display += "5";
         }else if (six.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "6";
+          display += "6";
         }else if (seven.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "7";
+          display += "7";
         }else if (eight.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "8";
+          display += "8";
         }else if (nine.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "9";
+          display += "9";
         }else if (dot.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += ".";
+          display += ".";
         }else if (equal.isUnderMouse(mouseX, mouseY)) {
-            
+          console.log(numbers)
+          answer = calculateAns(numbers, functions);
         }else if (plus.isUnderMouse(mouseX, mouseY)) {
-            functions = "+";
-            append(numbers, "");
+          append(functions, "+"); 
+          append(numbers, "");
+          display += "+";
         }else if (minus.isUnderMouse(mouseX, mouseY)) {
-            functions = "-";
             append(numbers, "");
+            append(functions, "-");
+          display += "-";
         }else if (multiply.isUnderMouse(mouseX, mouseY)) {
-            functions = "x";
             append(numbers, "");
+            append(functions, "x");
+          display += "x";
         }else if (divide.isUnderMouse(mouseX, mouseY)) {
-            functions = "÷";
             append(numbers, "");
+            append(functions, "÷");
+          display += "÷";
         }else if (power.isUnderMouse(mouseX, mouseY)) {
-            exponent = "2";
+            numbers[numbers.length-1] += "²";
+          display += "²";
         }else if (squareroot.isUnderMouse(mouseX, mouseY)) {
             numbers[numbers.length-1] += "√";
+          display += "√";
         }else if (reset.isUnderMouse(mouseX, mouseY)) {
             numbers = [""];
-            exponent = "";
-            functions = "";
+            functions = [];
+            display = "";
+            answer = "";
         }
 }
